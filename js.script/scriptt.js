b9d51e9c9b20bcb7378c2e1068914789102a8972
bjarte oliversen løken <Bjarteloken@hotmail.com>
2023-01-11 15:50:06 +0100

    init

---
+++ js.script/scriptt.js
@@ -0,0 +1,15 @@
+const opelAstra = {
+     brand: "Opel",
+     speed: 0,
+     revs: 0,
+     milage: 176455,
+     displacement: 1799,
+     running: false,
+     gearbox: ["Neutral", 1, 2, 3, 4, 5, "Reverse"],
+     ignition: function() {
+    opelAstra.on = !opelAstra.on
+    }
+     }
+
+     // optional task down below
+
